ea08b7b574ea59f17f15cd235d733fb71dfef1c5
willclarktech <will@willclark.tech>
2017-05-25 12:37:54 +0200

    don't reshape unnecessarily

--- screen_capture.py
+++ screen_capture.py
@@ -2,14 +2,16 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
 
 def screen_record():
+    last_time = time.time()
     while True:
         # 800x600 windowed mode
         printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
-        printscreen_numpy = np\
-            .array(printscreen_pil.getdata(), dtype='uint8')\
-            .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 4))
+        printscreen_numpy = np.array(printscreen_pil)
+        print 'loop took {} seconds'.format(time.time() - last_time)
+        last_time = time.time()
         cv2.imshow('window', cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
